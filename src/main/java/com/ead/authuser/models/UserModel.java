b39986b452619d63994884e95d0e87214bb74b33
BrunoWeberty <brunoweberty@hotmail.com>
2023-04-24 19:07:36 -0300

    Criação da tabela de relacionamento entre usuário e curso, além do service e repository desse relacionamento

--- src/main/java/com/ead/authuser/models/UserModel.java
+++ src/main/java/com/ead/authuser/models/UserModel.java
@@ -5,12 +5,14 @@ import com.ead.authuser.enums.UserType;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.springframework.hateoas.RepresentationModel;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Data
@@ -22,9 +24,9 @@ public class UserModel extends RepresentationModel<UserModel> implements Seriali
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID userId;
-    @Column(nullable = false, unique =  true, length = 50)
+    @Column(nullable = false, unique = true, length = 50)
     private String username;
-    @Column(nullable = false, unique =  true, length = 50)
+    @Column(nullable = false, unique = true, length = 50)
     private String email;
     @JsonIgnore
     @Column(nullable = false, length = 255)
@@ -49,4 +51,7 @@ public class UserModel extends RepresentationModel<UserModel> implements Seriali
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     @Column(nullable = false)
     private LocalDateTime lastUpdateDate;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private Set<UserCourseModel> usersCourses;
 }
