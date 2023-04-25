7cd614fd9944e894e5c5a820c9f4ac3fb348ad3b
BrunoWeberty <brunoweberty@hotmail.com>
2023-04-25 19:32:21 -0300

    criação da comunicação dos módulos de authuser e course através de uma api utilizando o Composition Pattern

--- src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
+++ src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
@@ -1,2 +1,16 @@
-package com.ead.authuser.configs;public class RestTemplateConfig {
+package com.ead.authuser.configs;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        //Do any additional configuration here
+        return builder.build();
+    }
 }
--- src/main/java/com/ead/authuser/services/UtilService.java
+++ src/main/java/com/ead/authuser/services/UtilService.java
@@ -1,2 +1,9 @@
-package com.ead.authuser.services;public interface UtilService {
+package com.ead.authuser.services;
+
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
+
+public interface UtilService {
+    String createUrl(UUID userId, Pageable pageable);
 }
