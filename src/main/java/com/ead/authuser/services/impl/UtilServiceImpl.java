0fd5fe8d0d0583d574ff617d516398105b2ed34c
BrunoWeberty <brunoweberty@hotmail.com>
2023-05-01 15:23:28 -0300

    Comunicação sincrona inscrição de user em course módulo de course 2

--- src/main/java/com/ead/authuser/services/impl/UtilServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UtilServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ead.authuser.services.impl;
 
 import com.ead.authuser.services.UtilService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +10,10 @@ import java.util.UUID;
 @Service
 public class UtilServiceImpl implements UtilService {
 
-    String REQUEST_URI = "http://localhost:8082";
+
 
     public String createUrl(UUID userId, Pageable pageable) {
-        return REQUEST_URI + "/courses?userId=" + userId + "&page=" + pageable.getPageNumber() + "&size="
+        return "/courses?userId=" + userId + "&page=" + pageable.getPageNumber() + "&size="
                 + pageable.getPageSize() + "&sort=" + pageable.getSort().toString().replaceAll(": ", ",");
     }
 }
